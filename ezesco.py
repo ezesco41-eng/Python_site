aab1dce3e66e9624c34853613b088139b7586b78
ezesco41-eng <ezesco41@gmail.com>
2026-06-04 10:43:55 +0100

    changes

---
+++ ezesco.py
@@ -0,0 +1,54 @@
+"""
+# var1 = "obi"
+# print(type(var1))
+# var1 = "A"
+# var2 = "a"
+# print(ord(var1))
+# print(ord(var2))
+
+# var1 = 25.5
+# # print(type(var1))
+# x = str(var1)
+# print(x)
+# print(type(x))
+
+# x=y=z="orange"
+# print(x)
+# print(y)
+# print(z)
+
+# x,y,z= "orange","banana","cherry"
+# print(x)
+# print(y)
+# print(z)
+
+#var1= "Eze"  # for loop
+# for e in var1:
+#     print(e)
+#print(len(var))
+# var2 = "Obi is a boy" #space count as a character,but not an apha numeric character
+# print(len(var2))
+# print("boy" in var2) 
+# print(var2[2:5])
+# print(var2[2:]  #This is slicing
+# print(var2[:3])
+# print(var2[1:2:8])
+# var3= "informationtechnology"
+# print(var3[1:8:2])
+# print(var3[1::3])
+"""
+list
+my_fruit = ["orange", "apple", "mango", "banana", "grape", "cocumber", "papaya", "watermelon", "guava"]
+print(my_fruit[4])
+name = "    obi is a boy      "
+print(name.strip()) 
+name = "    obi is a boy      "
+print(name.upper())
+name = "obi is a boy     "
+print(name.capitalize())
+
+# Concatenation
+
+name1 = "obi is a boy"
+name2 = "ada is a girl"
+print(name1 + " " + name2)
